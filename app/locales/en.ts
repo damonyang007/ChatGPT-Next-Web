396e6fca4526dcf2ea8bcbb70431647485930889
Damon Yang <53988941+damonyang007@users.noreply.github.com>
2024-03-26 17:35:03 +0800

    fix: en.ts

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -70,6 +70,7 @@ const en: LocaleType = {
       EnablePlugins: "Enable Plugins",
       DisablePlugins: "Disable Plugins",
       UploadImage: "Upload Images",
+      UploadFile: "Upload Files",
     },
     Rename: "Rename Chat",
     Typing: "Typing…",
